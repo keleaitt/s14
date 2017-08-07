999b93df042ece0fae1e17db2c4c8444867c8b70
YS <zibomail@gmail.com>
2017-08-07 22:35:54 +0800

    20170807

--- day4/装饰器.py
+++ day4/装饰器.py
@@ -2,8 +2,55 @@
 # -*- coding：utf-8 -*-
 # Author : Yuan
 
-def foo():
-    print("in the foo")
-    bar()
 
-foo()
+
+# def bar():
+#     print("in the bar")
+
+# def foo():
+#     print("in the foo")
+#     bar()
+# def bar():
+#     print("in the bar")
+# foo()
+#
+# def test1(func):
+#     print(func)
+#
+# def bar():
+#     print("in the bar")
+#
+#
+# test1(bar)
+# import time
+# def bar():
+#     time.sleep(3)
+#     print("in the bar")
+#
+#
+# def test1(func):
+#     kaishi_time = time.time()
+#     func()
+#     jieshu_time = time.time()
+#
+#     print("运行时间是：%s" %(jieshu_time-kaishi_time))
+#
+# test1(bar)
+
+import  time
+
+def bar():
+    time.sleep(3)
+    print("in the bar")
+
+def test1(func):
+    kashi_time=time.time()
+    print(func)
+    print("我套了一层")
+    jieshu_time = time.time()
+
+    return func
+
+bar = test1(bar)
+
+bar()
