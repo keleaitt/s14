628679e1548c276bae3bbd3445fea0430788394c
YS <zibomail@gmail.com>
2017-08-12 22:48:35 +0800

    20170807

---
+++ day4/生成器.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding：utf-8 -*-
+# Author : Yuan
+
+# a =  []
+#
+# for i in range(10):
+#     a.append(i*2)
+#
+# print(a)
+#
+#
+# b = [i*2 for i in range(10)]
+#
+# print(b)
+#
+# c = (i*2 for i in range(100000))  # 这就是生成器
+#
+# print(c)
+#
+# print(c.__next__())
+
+
+
+
