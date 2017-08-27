d6262dd32c5941ce6b59393fe436e5c6023d3a40
YS <zibomail@gmail.com>
2017-08-27 22:06:34 +0800

    20170807

---
+++ day4/json序列化.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding：utf-8 -*-
+# Author : Yuan
+
+
+
+f =  open("text.text","w")
---
+++ day4/内置函数.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding：utf-8 -*-
+# Author : Yuan
+
+# calc = lambda  n : 4 if  n>5 else 3
+#
+# print(calc(10) )
+
+# res = filter(lambda n:n+2,range(10))
+#
+# for i in res:
+#     print(i)
+
+
+# def add100(x):
+#     return  x+100
+#
+# hh = [11,22,33]
+#
+# jg = map(add100,hh)
+#
+#
+#
+#
+# import math
+# def is_sqr(x):
+#     return math.sqrt(x) % 1 == 0
+# print (filter(is_sqr, range(1, 101)))
+
+
+# list1 = [11,22,33,44,55,66]
+#
+# list2 = ['a','b','c','d','e']
+#
+# f = zip(list1,list2)
+#
+# for i  in f:
+#     print(i)
+
+__import__("吃包子")
